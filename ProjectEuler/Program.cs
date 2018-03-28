12451b4b7f68e3010a3f94e78f8d2e7fab8a9b9d
GmjvanGalen <gmjvangalen@gmail.com>
2018-03-29 00:28:37 +0200

    Solved problem 7

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -7,19 +7,43 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
+            int numbersToFind = 10001;
+            int numberToCheck = 2;
+            bool primeNumber;
 
-            int sumOfSquare = 0;
-            int squareOfSum = 0;
+            if (numbersToFind != 0) {
+                do
+                {
+                    primeNumber = true;
+                    for (int i = 2; i < numberToCheck; i++)
+                    {
+                        if (numberToCheck % i == 0)
+                        {
+                            primeNumber = false;
+                            numberToCheck++;
+                            break;
+                        }
 
-            for (int i = 1; i <= 100; i++)
-            {
-                sumOfSquare += i*i;
-                squareOfSum += i;
+                    }
+                    if (primeNumber) {
+                        numbersToFind--;
+                        if (numbersToFind == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            numberToCheck++;
+                        }
+                    }
+
+                    if (numbersToFind == 0) {
+                        break;
+                    }
+                } while (true);
             }
 
-            squareOfSum *= squareOfSum;
-
-            Console.WriteLine(squareOfSum - sumOfSquare);
+            Console.WriteLine($"Het 10001ste priemgetal = {numberToCheck}");
             Console.ReadKey();
         }
     }
