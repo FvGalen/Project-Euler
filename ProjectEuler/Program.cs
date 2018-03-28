56769e8ac792caf5a985f607e7d627b9de4d2b14
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 21:18:00 +0200

    Solved problem 1

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,16 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nummer = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if(i%3 == 0 || i%5 == 0){
+                    nummer = nummer + i;
+
+                }
+            }
+            Console.WriteLine(nummer);
         }
     }
 }
