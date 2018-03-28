aa9c005a7138674c65b409119c32d448701258a5
GmjvanGalen <gmjvangalen@gmail.com>
2018-03-28 23:53:05 +0200

    Solved problem 5

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -7,31 +7,28 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-           int answer = 0;
-           int answerx = 0;
-           int answery = 0;
 
-            for (int x = 100; x < 1000; x++)
+            int numberToCheck = 20;
+
+            do
             {
-                for (int y = 100; y < 1000; y++)
+                var succes = true;
+                for (int i = 1; i <= 20; i++)
                 {
-                    int sum = x * y;
-                    if(sum.ToString() == Reverse(sum.ToString()) && sum > answer){
-                        answer = sum;
-                        answerx = x;
-                        answery = y;
+                    if (numberToCheck % i != 0)
+                    {
+                        succes = false;
+                        numberToCheck++;
+                        break;
                     }
                 }
-            }
+                if (succes)
+                    break;
 
-            Console.WriteLine(answerx + " * " + answery + " = " + answer);
+            } while (true);
+
+            Console.WriteLine(numberToCheck);
             Console.ReadKey();
-        }
-
-        static string Reverse (string _input) {
-            var arr = _input.ToCharArray();
-            Array.Reverse(arr);
-            return new string(arr);
         }
     }
 }
