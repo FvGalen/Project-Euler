523740539f8738a28b71ef6392142a3ced48b438
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 21:30:17 +0200

    Solved problem 2

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,16 +6,21 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-            var nummer = 0;
+            var nummer1 = 1;
+            var nummer2 = 2;
 
-            for (int i = 1; i < 1000; i++)
-            {
-                if(i%3 == 0 || i%5 == 0){
-                    nummer = nummer + i;
+            var startnumber = 0;
+
+            while(nummer2 < 4000000){
+                if(nummer2%2 == 0){
+                    startnumber = startnumber + nummer2;
 
                 }
+                var tempnummer = nummer2;
+                nummer2 = nummer2 + nummer1;
+                nummer1 = tempnummer;
             }
-            Console.WriteLine(nummer);
+            Console.WriteLine(startnumber);
         }
     }
 }
