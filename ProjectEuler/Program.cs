2642cc990317a4c159b526e6fd426a3b4629a304
GmjvanGalen <gmjvangalen@gmail.com>
2018-03-29 14:10:48 +0200

    Tussenstand Sieve of Eranthoses class

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -7,37 +7,13 @@ namespace ProjectEuler
     {
         public static void Main(string[] args)
         {
-            long sumOfPrimes = 0;
-            long numberToCheck = 2;
-            bool primeNumber;
+            List<int> primeNumbers = SieveOfEranthoses.SieveOfEranthoses.FindPrimeNumbersInRange(2, 13);
 
-            do
+            for (int i = 0; i < primeNumbers.Count; i++)
             {
-                if (numberToCheck > 2000000)
-                    break;
-
-                if(numberToCheck % 100000 == 0)
-                    Console.WriteLine($"sum of primes is: {sumOfPrimes} when the number to check is {numberToCheck}");
-
-                primeNumber = true;
-                for (int i = 2; i < numberToCheck; i++)
-			    {
-                    if(numberToCheck % i == 0){
-                        primeNumber = false;
-                        numberToCheck ++;
-                        break;
-                     }
-                }
-                
-                if(primeNumber){
-                    sumOfPrimes += numberToCheck;
-                    numberToCheck++;
-                }
-
-
-	        } while (true);
-
-            Console.WriteLine($"sum of primes is: {sumOfPrimes}");
+                Console.WriteLine(primeNumbers[i]);
+            }
+            Console.WriteLine("klaar");
             Console.ReadKey();
         }
     }
---
+++ ProjectEuler/SieveOfEranthoses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEranthoses
+{
+    class SieveOfEranthoses
+    {
+        public static List<int> FindPrimeNumbersInRange(int _startNumber, int _endNumber)
+        {
+            List<int> primeNumbers = new List<int>();
+
+            for (int i = _startNumber; i <= _endNumber; i++)
+            {
+                primeNumbers.Add(i);
+            }
+
+            return primeNumbers;
+        }
+    }
+}
